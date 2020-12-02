0ae4f2b2ee5fcfc114a6b56d06f78f1512768c8f
Deny Pătrașcu <denypatrascu@gmail.com>
2020-12-02 14:36:50 +0200

    new cardinal distribution

--- cardinal/index-34b70e38.js
+++
@@ -1,8 +0,0 @@
-import { A as ActiveRouter } from './active-router-44a70032.js';
-import './match-path-760e1797.js';
-
-function injectHistory(Component) {
-    ActiveRouter.injectProps(Component, ['history', 'location']);
-}
-
-export { injectHistory as i };
--- cardinal/index-f0762390.js
+++
@@ -1,8 +0,0 @@
-import { A as ActiveRouter } from './active-router-89a25b91.js';
-import './match-path-760e1797.js';
-
-function injectHistory(Component) {
-    ActiveRouter.injectProps(Component, ['history', 'location']);
-}
-
-export { injectHistory as i };
